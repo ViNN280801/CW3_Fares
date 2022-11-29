fb8350dacae82aab28b4863ab9b06d236b642da6
Vladislav Semykin <vladislav_semykin01@mail.ru>
2022-11-29 08:34:40 +0300

    mirroringBits() fixed

--- task2.c
+++ task2.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <time.h>
 
-static const short int BITS = 8U;
+static const short int BITS_IN_ONE_BYTE = 8U;
 
 /* Обрабатывает ошибку ввода пользователем символа или строки,
 когда его просят ввести число. Возвращает число, введённое пользователем */
@@ -26,14 +26,29 @@ int inputInt()
     return atoi(buf);
 }
 
-unsigned int mirroringBits(unsigned int value)
+/* Принимает целое число без знака (для примера взят тип unsigned char)
+чтобы значения варьировались от 0 до 255
+Возвращает число, полученное отзеркаливанием битов
+Пример:
+10010100 -> 00101001 (2)
+148 -> 41 (10)
+224 -> 51 (8)
+ */
+unsigned char mirroringBits(unsigned char value)
 {
-    unsigned int result = 0;
-    for (size_t i = 0; i < (sizeof(value) * BITS); i++)
+    unsigned char result = 0;
+
+    /* Итерация по битам в размере типа 'unsigned char'
+    То есть 'sizeof(unsigned char)' = 1 байт, так как в 1 байте 8 бит, то умножаем
+    количество байт на количество битов в 1-ом байте */
+    for (size_t i = 0; i < (sizeof(value) * BITS_IN_ONE_BYTE); i++)
     {
-        if ((value & (1 << i)))
-            result |= 1 << (((sizeof(value) * BITS) - 1) - i);
+        int bit = value & 1;
+        result <<= 1;
+        result += bit;
+        value >>= 1;
     }
+
     return result;
 }
 
@@ -49,7 +64,7 @@ int main(void)
     while (true)
     {
         printf("1. Начало работы\n2. Выход\nВвод: ");
-        menu_var= inputInt();
+        menu_var = inputInt();
 
         if (menu_var == 1)
         {
@@ -61,9 +76,10 @@ int main(void)
                 if (submenu_var == 1)
                 {
                     printf("Введите число: ");
-                    unsigned int number = inputInt();
-                    unsigned int mirror = mirroringBits(number);
-                    printf("%u\t%u\n", number, mirror);
+                    unsigned char number = inputInt();
+                    unsigned char mirror = mirroringBits(number);
+                    printf("Десятичная СС:\n%u\t%u\n", number, mirror);
+                    printf("Восьмеричная СС:\n%o\t%o\n", number, mirror);
                 }
                 else if (submenu_var == 2)
                 {
